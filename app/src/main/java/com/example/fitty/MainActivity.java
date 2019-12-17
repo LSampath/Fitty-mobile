dab37635920bbf79bd083c82a3ea61174895e193
Lahiru Sampath <lahirusampath.15@cse.mrt.ac.lk>
2019-12-17 22:07:13 +0530

    Dec 17 - Init Commit

---
+++ app/src/main/java/com/example/fitty/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.fitty;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.navbar_bottom);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        setFragment(HomeFragment.newInstance(null, null));
+                        break;
+                    case R.id.action_pulse:
+                        setFragment(HeartRateFragment.newInstance(null, null));
+                        break;
+                    case R.id.action_run:
+                        setFragment(RunningFragment.newInstance(null, null));
+                        break;
+                    case R.id.action_history:
+                        setFragment(HistoryFragment.newInstance(null, null));
+                        break;
+                }
+                return true;
+            }
+        });
+
+        bottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                //
+            }
+        });
+
+        setFragment(HomeFragment.newInstance(null, null));
+    }
+
+    private void setFragment(Fragment object) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.layout_container, object);
+//        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+}
