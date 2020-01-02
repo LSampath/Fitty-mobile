8cc20ac7841af7a4b7a9603eee8c970eecf5e948
Ashan <ashan.15@cse.mrt.ac.lk>
2020-01-02 15:30:56 +0530

    Jan 2 - Init commit

--- app/src/main/java/com/example/fitty/views/HeartRateFragment.java
+++ app/src/main/java/com/example/fitty/views/HeartRateFragment.java
@@ -1,20 +1,29 @@
 package com.example.fitty.views;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.PowerManager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.fitty.R;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 /**
@@ -29,6 +38,43 @@ public class HeartRateFragment extends Fragment {
     private BroadcastReceiver broadcastReceiver;
 
     private TextView titleView;
+
+    //////////////////////////////////////////////
+    private static final String TAG = "HeartRateMonitor";
+    private static final AtomicBoolean processing = new AtomicBoolean(false);
+
+    private static SurfaceHolder previewHolder = null;
+    private static Camera camera = null;
+    @SuppressLint("StaticFieldLeak")
+    private static View image = null;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView text = null;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView imgavgtxt = null;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView rollavgtxt = null;
+
+    private static PowerManager.WakeLock wakeLock = null;
+
+    private static int averageIndex = 0;
+    private static final int averageArraySize = 4;
+    private static final int[] averageArray = new int[averageArraySize];
+
+    public static enum TYPE {
+        GREEN, RED
+    };
+    private static TYPE currentType = TYPE.GREEN;
+
+    public static TYPE getCurrent() {
+        return currentType;
+    }
+
+    private static int beatsIndex = 0;
+    private static final int beatsArraySize = 3;
+    private static final int[] beatsArray = new int[beatsArraySize];
+    private static double beats = 0;
+    private static long startTime = 0;
+    /////////////////////////////////////////////
 
     public HeartRateFragment() {
     }
@@ -49,19 +95,188 @@ public class HeartRateFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_heart_rate, container, false);
 
-        this.titleView = view.findViewById(R.id.fragment_heart_tv_title);
+//        this.titleView = view.findViewById(R.id.fragment_heart_tv_title);
+    ///////////////////////////////////////////////
+        SurfaceView preview = (SurfaceView) view.findViewById(R.id.preview);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
+        image = view.findViewById(R.id.image);
+        text = view.findViewById(R.id.text);
+        imgavgtxt = view.findViewById(R.id.img_avg_text);
+        rollavgtxt = view.findViewById(R.id.rollavg_text);
 
-        broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                titleView.setText(intent.getExtras().get("value").toString());
-            }
-        };
-        getActivity().registerReceiver(broadcastReceiver, new IntentFilter("just_an_intent"));
-
+        PowerManager pm = (PowerManager) getActivity().getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "DoNot:DimScreen");
+    ///////////////////////////////////////////////////
         return view;
     }
+    /////////////////////////////////////////////////////
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        wakeLock.acquire(10*60*1000L /*10 minutes*/);
+
+        camera = Camera.open();
+
+        startTime = System.currentTimeMillis();
+    }
+    public void onPause() {
+        super.onPause();
+
+        wakeLock.release();
+
+        camera.setPreviewCallback(null);
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+    private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onPreviewFrame(byte[] data, Camera cam) {
+            if (data == null) throw new NullPointerException();
+            Camera.Size size = cam.getParameters().getPreviewSize();
+            if (size == null) throw new NullPointerException();
+
+            if (!processing.compareAndSet(false, true)) return;
+
+            int width = size.width;
+            int height = size.height;
+
+            int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
+            // Log.i(TAG, "imgAvg="+imgAvg);
+            if (imgAvg == 0 || imgAvg == 255) {
+                processing.set(false);
+                return;
+            }
+
+            int averageArrayAvg = 0;
+            int averageArrayCnt = 0;
+            for (int i = 0; i < averageArray.length; i++) {
+                if (averageArray[i] > 0) {
+                    averageArrayAvg += averageArray[i];
+                    averageArrayCnt++;
+                }
+            }
+
+            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+            TYPE newType = currentType;
+
+            imgavgtxt.setText("image average:"+ Integer.toString(imgAvg));
+            rollavgtxt.setText("rolling average:"+ Integer.toString(rollingAverage));
+            if (imgAvg < rollingAverage) {
+                newType = TYPE.RED;
+                if (newType != currentType) {
+                    beats++;
+                    // Log.d(TAG, "BEAT!! beats="+beats);
+                }
+            } else if (imgAvg > rollingAverage) {
+                newType = TYPE.GREEN;
+            }
+
+            if (averageIndex == averageArraySize) averageIndex = 0;
+            averageArray[averageIndex] = imgAvg;
+            averageIndex++;
+
+            // Transitioned from one state to another to the same
+            if (newType != currentType) {
+                currentType = newType;
+                image.postInvalidate();
+            }
+
+            long endTime = System.currentTimeMillis();
+            double totalTimeInSecs = (endTime - startTime) / 1000d;
+            if (totalTimeInSecs >= 10) {
+                double bps = (beats / totalTimeInSecs);
+                int dpm = (int) (bps * 60d);
+                if (dpm < 30 || dpm > 180) {
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                    processing.set(false);
+                    return;
+                }
+
+                // Log.d(TAG,
+                // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
+
+                if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                beatsArray[beatsIndex] = dpm;
+                beatsIndex++;
+
+                int beatsArrayAvg = 0;
+                int beatsArrayCnt = 0;
+                for (int i = 0; i < beatsArray.length; i++) {
+                    if (beatsArray[i] > 0) {
+                        beatsArrayAvg += beatsArray[i];
+                        beatsArrayCnt++;
+                    }
+                }
+                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+                text.setText(String.valueOf(beatsAvg)+" bpm");
+                startTime = System.currentTimeMillis();
+                beats = 0;
+            }
+            processing.set(false);
+        }
+    };
+
+    private static SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+public void surfaceCreated(SurfaceHolder holder) {
+    try {
+        camera.setPreviewDisplay(previewHolder);
+        camera.setPreviewCallback(previewCallback);
+    } catch (Throwable t) {
+        Log.e("Preview-surfaceCallback", "Exception in setPreviewDisplay()", t);
+    }
+}
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        Camera.Size size = getSmallestPreviewSize(width, height, parameters);
+        if (size != null) {
+            parameters.setPreviewSize(size.width, size.height);
+            Log.d(TAG, "Using width=" + size.width + " height=" + size.height);
+        }
+        camera.setParameters(parameters);
+        camera.startPreview();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Ignore
+    }
+};
+    private static Camera.Size getSmallestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea < resultArea) result = size;
+                }
+            }
+        }
+
+        return result;
+    }
+        /////////////////////////////////////////////////////
 
     @Override
     public void onAttach(Context context) {
